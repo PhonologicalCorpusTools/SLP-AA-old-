89d0ec721c2fd870ab849531fb92b101f466e85f
Li <lize.works@gmail.com>
2018-05-21 18:31:19 -0700

    Added splitter

--- slpa/analyze.py
+++ slpa/analyze.py
@@ -11,11 +11,26 @@ def readMyFile(filename):
         next(csvReader)
         
         for row in csvReader:
+            front_removed = row[5:]
+            both_removed = front_removed[:len(front_removed)-7]
+            word_dict[row[0]]=both_removed
 
-            word_dict[row[0]]=row[5:]
-            
- 
+def split_arr_fourths(arr):
+    config1hand1 = []
+    config1hand2 = []
+    config2hand1 = []
+    config2hand2 = []
+
+    config1hand1 = arr[0:int(len(arr)/4)]
+    config1hand2 = arr[int(len(arr)/4):int(2*len(arr)/4)]
+    config2hand1 = arr[int(2*len(arr)/4):int(3*len(arr)/4)]
+    config2hand2 = arr[int(3*len(arr)/4):int(len(arr))]
+
+    return [config1hand1,config1hand2,config2hand1,config2hand2]
+    
+
+
  
 readMyFile('export_ASL.csv')
 
-print(word_dict['1_DOLLAR'])
+
