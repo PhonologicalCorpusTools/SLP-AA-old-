d9d86f8f74149cf9374955f10f46765504f370e6
Li <lize.works@gmail.com>
2018-05-26 17:47:11 -0700

    Made it case sensitive proof as well as made it print total summary

--- slpa/analyze.py
+++ slpa/analyze.py
@@ -1,7 +1,8 @@
 import csv
+import os
 import numpy as np
 
-parts_list = ["All","Forearm","Thumb","Thumb / Finger Contact","Index Finger","Middle Finger","Ring Finger","Pinky Finger","Thumb / Finger Surfaces","Finger Contact","Extensions","Finger 1 Extensions","Finger 2 Extensions","Finger 3 Extensions","Finger 4 Extensions","Finger / Finger Contact","Proximal Joints","Medial Joints","Distal Joints"]
+parts_list = ["All","Forearm","Thumb","Thumb / Finger Contact","Index Finger","Middle Finger","Ring Finger","Pinky Finger","Thumb / Finger Surfaces","Finger Contact","Extensions","Finger 1 Extensions","Finger 2 Extensions","Finger 3 Extensions","Finger 4 Extensions","Finger / Finger Contact","Proximal Joints","Medial Joints","Distal Joints","ALL summary of 1-19"]
 conf_list = ["Config-1 Hand-1","Config-1 Hand-2","Config-2 Hand-1","Config-2 Hand-2"]
 
 def delete_last_two(arr):
@@ -75,29 +76,39 @@ def print_options_get_val(dict1,dict2):
     word_val =""
     while True:
         try:
-            word_val = input("Type in the word you'd like to compare: ")
+            word_val = input("Type in the word you'd like to compare (Not case-sensitive): ")
+            word_val = word_val.upper()
             dict1[word_val]
             dict2[word_val]
             break
         except KeyError:
-            print("Word is not located in excel sheet")
+            print("Word is not located in one or both excel sheets.")
     print("\n")
 
     print("Choose from 4 types of combinations below")
     numb0 = 0
+    combined_list0 = ""
     for elem in conf_list:
         numb0 += 1
-        print(numb0, ".", elem)
+        combined_list0 += str(numb0)+". "+elem
+        if numb0 != 4:
+            combined_list0 += "\n"
+    print(combined_list0)
     config_val = weird_function("1-4")
     config_val -=1
     print("\n")
     
-    print("Type in a value from 1-19 for the reliability statistic you'd like to view")
+    print("Type in a value from 1-20 for the reliability statistic you'd like to view")
     numb = 0
+    combined_list = ""
     for elem in parts_list:
         numb +=1
-        print(numb, ".", elem)
-    part_val = weird_function("1-19")
+        combined_list += str(numb)+ ". "+ elem
+        if numb != 20:
+            combined_list += "\n"
+            
+    print(combined_list)
+    part_val = weird_function("1-20")
     print("\n")
 
     return word_val, config_val, part_val
@@ -132,41 +143,68 @@ def get_range(numb):
 # config index = [0 for config1hand1] [1 for config1hand2] [2 for config2hand1] [3 for config2hand2]
 def reliability_analysis(dict1,dict2):
     word_ch, config_ch, part_ch = print_options_get_val(dict1,dict2)
+
+    print("Word: ",word_ch)
+    print("Configuration: ",conf_list[config_ch])
+    print("\n")
     
     mutated_arr1 = ["*"] + mutate_constants(dict1[word_ch][config_ch])
     mutated_arr2 = ["*"] + mutate_constants(dict2[word_ch][config_ch])
 
+    if part_ch >=1 and part_ch <= 19:
+        range_list = get_range(part_ch)
+        single_number_list = []
 
-    range_list = get_range(part_ch)
-    single_number_list = []
+        if (len(range_list) == 3) and ("-" in range_list):
+            single_number_list = list(range(range_list[0],range_list[2]+1))
+        elif len(range_list) != 0:
+            single_number_list = range_list
 
-    if (len(range_list) == 3) and ("-" in range_list):
-        single_number_list = list(range(range_list[0],range_list[2]+1))
-    elif len(range_list) != 0:
-        single_number_list = range_list
+        dict1_compare_list = []
+        dict2_compare_list = []
+        for ind in single_number_list:
+            dict1_compare_list.append(mutated_arr1[ind])
+            dict2_compare_list.append(mutated_arr2[ind])
 
-    dict1_compare_list = []
-    dict2_compare_list = []
-    for ind in single_number_list:
-        dict1_compare_list.append(mutated_arr1[ind])
-        dict2_compare_list.append(mutated_arr2[ind])
+        if '*' in dict1_compare_list and dict2_compare_list:
+            dict1_compare_list=list(filter(lambda a: a != "*", dict1_compare_list))
+            dict2_compare_list=list(filter(lambda a: a != "*", dict2_compare_list))
 
-    if '*' in dict1_compare_list and dict2_compare_list:
-        dict1_compare_list=list(filter(lambda a: a != "*", dict1_compare_list))
-        dict2_compare_list=list(filter(lambda a: a != "*", dict2_compare_list))
-    print("Word: ",word_ch)
-    print("Configuration: ",conf_list[config_ch])
-    print("Section: ",parts_list[part_ch-1]) 
-    print("\n")
-    print("Reliability = ", (1-np.mean(np.array(dict1_compare_list) != np.array(dict2_compare_list)))*100,"%")
+        print("Section: ",parts_list[part_ch-1]) 
+        print("Reliability = ", (1-np.mean(np.array(dict1_compare_list) != np.array(dict2_compare_list)))*100,"%")
+    elif part_ch == 20:
+        combined_print = ""
+        for part in range(1,len(parts_list)):
+            range_list = get_range(part)
+            single_number_list = []
+
+            if (len(range_list) == 3) and ("-" in range_list):
+                single_number_list = list(range(range_list[0],range_list[2]+1))
+            elif len(range_list) != 0:
+                single_number_list = range_list
+
+            dict1_compare_list = []
+            dict2_compare_list = []
+            for ind in single_number_list:
+                dict1_compare_list.append(mutated_arr1[ind])
+                dict2_compare_list.append(mutated_arr2[ind])
+
+            if '*' in dict1_compare_list and dict2_compare_list:
+                dict1_compare_list=list(filter(lambda a: a != "*", dict1_compare_list))
+                dict2_compare_list=list(filter(lambda a: a != "*", dict2_compare_list))
+
+            combined_print += "Section: "+parts_list[part-1]+"\n"+ "Reliability = "+ str((1-np.mean(np.array(dict1_compare_list) != np.array(dict2_compare_list)))*100)+"%"+"\n"+"\n"
+        print(combined_print)
+            
 
     print("\n")
     print("Do you want to search another word?")
-    print("1. Search another word")
-    print("2. Exit")
+    print("1. Yes")
+    print("2. No")
     do_exit = weird_function("1-2")
 
     if do_exit == 1:
+        print("\n" * 80)
         reliability_analysis(dict1,dict2)
     elif do_exit == 2:
         exit(0)
